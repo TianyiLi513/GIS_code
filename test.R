ce9bd1de8a367a54997d5f8e188ddb4e7bda3b3e
TianyiLi513 <ucfniax.ucl.ac.uk>
2021-12-14 16:12:32 +0000

    5th

--- test.R
+++ test.R
@@ -1,9 +1,10 @@
 test 
 library(here)
-library(gitcreds)
-gitcreds_set()
+
 edit_git_config()
 library(usethis)
-
+library(gitcreds)
+gitcreds_set()
 create_github_token()
 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
+
